f37df901a69491676498014ac5c0256fa6bc24ea
ayano-w <ayasaku.t5@gmail.com>
2021-12-11 15:20:16 +0000

    add routind

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,33 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'nurses/index'
-  end
-  namespace :admin do
-    get 'patients/new'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :nurse do
-    get 'reviews/index'
-    get 'reviews/edit'
-  end
-  namespace :nurse do
-    get 'tasks/new'
-    get 'tasks/edit'
-  end
-  namespace :nurse do
-    get 'schedules/show'
-    get 'schedules/index'
-    get 'schedules/create'
-    get 'schedules/destroy'
-  end
-  namespace :nurse do
-    get 'nurses/show'
-  end
-  namespace :nurse do
-    get 'homes/top'
-  end
+
   #管理者用
   devise_for :admins,skip: [:passwords,], controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions"
   }
 
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :patients, only: [:new, :create, :destroy]
+    resources :nurses, only: [:index, :update]
+  end
+
+
   #看護師用
   devise_for :nurses,skip: [:passwords,], controllers: {
     registrations: "nurse/registrations",
     sessions: "nurse/sessions"
   }
+
+  scope module: :nurse do
+    get 'top' => 'homes#top', as: 'top'
+    resources :nurses, only: [:show, :update]
+    resources :schedules, only: [:show, :index, :create, :destroy] do
+      resources :reviews, only: [:index, :create, :edit, :update, :destroy]
+    end
+    resources :task_lists, only: [:create, :destroy]
+    resources :tasks, expect: [:index, :show]
+  end
 
 end
