842fac4cb0bf87e2f4b02cb8446db103e98e6e6d
ayano-w <ayasaku.t5@gmail.com>
2021-12-13 01:40:43 +0000

    fix devise and add mypage

--- app/controllers/nurse/nurses_controller.rb
+++ app/controllers/nurse/nurses_controller.rb
@@ -1,4 +1,18 @@
 class Nurse::NursesController < ApplicationController
   def show
+    @nurse = current_nurse
   end
+
+  def update
+    nurse = current_nurse
+    nurse.update(nurse_params)
+    redirect_to mypage_path
+  end
+
+  private
+
+  def nurse_params
+    params.require(:nurse).permit(:name, :ward_id)
+  end
+
 end
