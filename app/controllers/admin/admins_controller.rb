aa49943967ae075e29c6206e616a52641490ecf2
ayano-w <ayasaku.t5@gmail.com>
2021-12-13 02:54:37 +0000

    fix admin edit page

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,4 +1,18 @@
 class Admin::AdminsController < ApplicationController
   def edit
+    @admin = current_admin
   end
+
+  def update
+    admin = current_admin
+    admin.update(admin_params)
+    redirect_to edit_admin_admin_path
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:name, :ward_id)
+  end
+
 end
