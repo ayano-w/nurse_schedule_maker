f0bee63a7e21a5e09a9a42695a1d420887779aba
ayano-w <ayasaku.t5@gmail.com>
2021-12-13 09:07:48 +0000

    add admin/edit and admin/nurse and admin/top. but admin/nurses controller have some errors

--- app/controllers/admin/nurses_controller.rb
+++ app/controllers/admin/nurses_controller.rb
@@ -1,4 +1,29 @@
 class Admin::NursesController < ApplicationController
   def index
+    admin_ward_id = current_admin.ward_id
+    @nurses = Nurse.where(ward_id: admin_ward_id)
+
   end
+
+  def update
+    nurses = Nurse.find(nurses_params)
+    # @nurses = nurses_params.map do |id, nurse_param|
+    #   nurse = Nurse.find(id)
+      nurses.update_all
+    # end
+    redirect_to admin_nurses_path
+  end
+
+
+  private
+  def nurse_params
+    params.require(:nurse).permit(:name, :ward_id)[:nurses]
+  end
+
+  def nurses_params
+    params.require(:nurse).permit(nurses: [:attendance])[:nurses]
+  end
+
+
+
 end
