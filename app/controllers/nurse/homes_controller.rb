182a52619361e7d202e3ed908ea4fe121847674d
ayano-w <ayasaku.t5@gmail.com>
2021-12-13 14:38:46 +0000

    add nurse top page

--- app/controllers/nurse/homes_controller.rb
+++ app/controllers/nurse/homes_controller.rb
@@ -1,4 +1,8 @@
 class Nurse::HomesController < ApplicationController
   def top
+    if nurse_signed_in?
+      @nurses = Nurse.where(ward_id: current_nurse.ward_id)
+      @nurses = @nurses.where(attendance: true)
+    end
   end
 end
