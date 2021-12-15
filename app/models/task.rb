050df3baede88759ea4768d92a9b598e11092362
ayano-w <ayasaku.t5@gmail.com>
2021-12-15 04:11:57 +0000

    add method to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,11 @@ class Task < ApplicationRecord
     less_than_or_equal_to: 3,
     greater_than_or_equal_to: 1}, presence: true
 
+
+  #タスクリスト（スケジュールの中の各患者の行）に関連するタスクを集めるメソッド
+  def self.collect_tasks(task_list)
+    where(task_list_id: task_list.id)
+  end
+
+
 end
