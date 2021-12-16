cb990a48705c77d579d7d3811669445d4d053816
ayano-w <ayasaku.t5@gmail.com>
2021-12-16 15:50:26 +0000

    fix task position

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,5 +14,11 @@ class Task < ApplicationRecord
   def self.collect_tasks(task_list)
     where(task_list_id: task_list.id)
   end
-  
+
+  def calculation_task_position(task)
+    ((task.task_start_time.seconds_since_midnight) - (60*60*8)) / (60*60*13) * 100
+  end
+
+
+
 end
