28a1d57ac3092a7b02cf624bbf49293838f8b578
ayano-w <ayasaku.t5@gmail.com>
2021-12-23 14:52:19 +0000

    add test cpntents

--- spec/models/admin.rb
+++ spec/models/admin.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe Admin, type:model do
+  describe 'モデルのテスト' do
+    it "有効なadminの場合は保存されるか" do
+      expect(build(:amin)).to be_valid
+    end 
+    
+    context "空白のバリデーションチェック" do
+      it "nameが空白の場合にエラーメッセージが返ってくるか" do
+      admin = build(:admin, name: nil)
+      admin.valid?
+  end
+end
