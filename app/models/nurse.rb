ebe0979126cfaf9152baac0ac25166b0ef2ca1fa
ayano-w <ayasaku.t5@gmail.com>
2022-01-31 05:10:08 +0000

    fix nurse.rb

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -16,16 +16,16 @@ class Nurse < ApplicationRecord
 
 
   #emailの設定をdeviseから除外する
-   def email_required?
-     false
-   end
+  def email_required?
+    false
+  end
 
-   def email_changed?
-     false
-   end
+  def email_changed?
+    false
+  end
 
   # ログイン看護師と同じ病棟かつ出勤日の看護師を抽出
   def self.attendance_nurse_ids(ward_id)
-      where(ward_id: ward_id, attendance: true).pluck(:id)
+    where(ward_id: ward_id, attendance: true).pluck(:id)
   end
 end
